a384ec69ee316f3096cf37249a88bf77fa4936d0
SC100825837 <100825837@qq.com>
2021-09-15 16:56:19 +0800

    读取配置文件工具类

---
+++ src/main/java/com/jc/research/util/PropertiesUtil.java
@@ -0,0 +1,29 @@
+package com.jc.research.util;
+
+import lombok.Data;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.support.EncodedResource;
+import org.springframework.core.io.support.PropertiesLoaderUtils;
+
+import java.io.IOException;
+import java.util.Properties;
+
+@Data
+public class PropertiesUtil {
+    private static Logger logger = LoggerFactory.getLogger(PropertiesUtil.class);
+
+    public static Properties getProperties(String location) {
+        Properties properties = null;
+        try {
+            logger.info("加载资源[{}]", location);
+            properties = PropertiesLoaderUtils.loadProperties(new EncodedResource(new ClassPathResource(location), "UTF-8"));
+        } catch (IOException e) {
+            logger.error("加载资源[{}]失败", location);
+            logger.error(e.getMessage());
+            e.printStackTrace();
+        }
+        return properties;
+    }
+}
